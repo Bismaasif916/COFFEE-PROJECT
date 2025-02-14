02c368cf8d61297e15ff747aad991bb6af812dae
Bismaasif916 <bismaasif916@gmail.com>
2025-02-15 00:07:10 +0500

    initial changes

---
+++ assets/js/topcards.js
@@ -0,0 +1,61 @@
+{/* <nav class="navbar navbar-expand-lg" id="navbar">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#" id="logo"><img src="assets/images/l_3-removebg-preview.png" alt="" style="height: 65px; width: 65px; margin-left: 10px;">offee Shop</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span><i class="fa-solid fa-bars" style="color: white; font-size: 25px;"></i></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active " aria-current="page" href="#home">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#menu">Menu</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#product">Product</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#gallery">Gallery</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#blogs">Blogs</a>
+                  </li>
+                </ul>
+                <form class="d-flex me-3">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-dark" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+         */}
+        //  <section id="top-cards">
+        //       <div class="heading-2">Top <span>Categories</span></div>
+        //       <div class="container">
+        //         <div class="row">
+        //           <div class="col-md-4 py-3 py-md-0">
+        //           <div class="cards">
+        //             <img src="assets/images/c19jpg.jpg" alt="" width="420px" height="300px">
+        //            </div>
+        //         </div>
+        //           <div class="col-md-4 py-3 py-md-0">
+        //           <div class="cards">
+        //             <img src="assets/images/top cd 6.jpg" alt="" width="420px" height="300px">
+        //            </div>
+        //         </div>
+        //           <div class="col-md-4 py-3 py-md-0">
+        //           <div class="cards">
+        //             <img src="assets/images/c2.jpg" alt="" width="420px" height="300px">
+        //            </div>
+        //         </div>
+        //         </div>
+        //       </div>
+        //         </section>
+             
